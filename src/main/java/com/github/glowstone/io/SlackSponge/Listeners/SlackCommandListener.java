0fc7ff83b8a5446fa5a8f25d1611097d91fdf1e5
paulkubi <paulkubitschek@gmail.com>
2016-06-17 17:55:20 -0500

    Add command line "/mc " to list players on server

--- src/main/java/com/github/glowstone/io/SlackSponge/Listeners/SlackCommandListener.java
+++ src/main/java/com/github/glowstone/io/SlackSponge/Listeners/SlackCommandListener.java
@@ -2,7 +2,11 @@ package com.github.glowstone.io.SlackSponge.Listeners;
 
 import com.github.glowstone.io.SlackSponge.Events.SlackCommandEvent;
 import com.github.glowstone.io.SlackSponge.Server.SlackSender;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.Listener;
+
+import java.util.Collection;
 
 public class SlackCommandListener {
 
@@ -14,7 +18,14 @@ public class SlackCommandListener {
         switch (event.getCommand().toLowerCase().substring(1)) {
 
             case COMMAND_MC:
-                // do mc command here
+                Collection<Player> players = Sponge.getGame().getServer().getOnlinePlayers();
+                StringBuilder message = new StringBuilder();
+                message.append(players.size() + "Player(s) ");
+
+                for (Player p : players) {
+                    message.append(p.getName());
+                }
+                SlackSender.getInstance().sendCommandResponse(message.toString());
                 break;
 
             default:
