9326eb6d41791a802d83be70589ba76fde571e45
paulkubi <paulkubitschek@gmail.com>
2016-06-17 13:41:49 -0500

    Updated version to 1.0.0

--- src/main/java/com/github/glowstone/io/SlackSponge/SlackSponge.java
+++ src/main/java/com/github/glowstone/io/SlackSponge/SlackSponge.java
@@ -23,7 +23,7 @@ public class SlackSponge {
 
     private static SlackSponge instance;
     public static final String NAME = "SlackSponge";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.0";
 
     @Inject
     private Game game;
